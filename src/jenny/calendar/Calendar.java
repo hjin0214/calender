7667fff5ca1c8e79bfc25aaaa99953b138231721
변희진 <hjbyoun@joeunins.com>
2022-07-26 19:24:12 +0900

    method() 방식 추가

--- src/jenny/calendar/Calendar.java
+++ src/jenny/calendar/Calendar.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	// 함수 이용
+			int maxdays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			
+			public int getMaxdays(int mth) {
+				return maxdays[mth -1];
+			}
+			
 	public static void main(String[] args) {
+		
 		
 		// 입력 받은 달의 최대 일수 출력
 		
 		Scanner scanner = new Scanner(System.in);
+		Calendar calendar = new Calendar();
 		
 		System.out.println("원하는 달을 입력하세요.");
 		int mth = scanner.nextInt();
 		
+		System.out.printf("%d 월은 %d일까지 있습니다.", mth, calendar.getMaxdays(mth));
+		
 		// 배열 이용
-		int month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d 월은 %d일까지 있습니다.", mth, month[mth-1]);
+//		int month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//		System.out.printf("%d 월은 %d일까지 있습니다.", mth, month[mth-1]);
 		
 		// if문 이용
 //		if(mth%2 == 1) {
